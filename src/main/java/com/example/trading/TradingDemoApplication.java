d2104fbbde96e777dde731dda8e66b9665c7cf08
Tom Donohue <monodot@gmail.com>
2022-10-20 15:55:13 +0200

    Initial commit from Initializr

---
+++ src/main/java/com/example/trading/TradingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingDemoApplication.class, args);
+	}
+
+}
